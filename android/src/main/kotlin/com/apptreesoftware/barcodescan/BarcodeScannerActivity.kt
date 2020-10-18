4a30fc8763676a1ba1f1544834e863b78e271933
GeorgeReda <gogoreda01@gmail.com>
2020-10-18 19:23:10 +0200

    Update BarcodeScannerActivity.kt

--- android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
+++ android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
@@ -31,6 +31,46 @@ class BarcodeScannerActivity : Activity(), ZXingScannerView.ResultHandler {
         // this paramter will make your HUAWEI phone works great!
         scannerView.setAspectTolerance(0.5f)
         setContentView(scannerView)
+        if (!setSecureSurfaceView()) {
+            Log.e("MainActivity", "Could not secure the MainActivity!")
+            // React as appropriate.
+        }
+        getWindow().addFlags(LayoutParams.FLAG_SECURE)
+        getWindow().setFlags(LayoutParams.FLAG_SECURE,
+                LayoutParams.FLAG_SECURE)
+        SurfaceView(applicationContext).setSecure(true)
+    }
+    private fun setSecureSurfaceView(): Boolean {
+        val content = findViewById<ViewGroup>(android.R.id.content)
+        if (!isNonEmptyContainer(content)) {
+            return false
+        }
+        val splashView = content.getChildAt(0)
+        if (!isNonEmptyContainer(splashView)) {
+            return false
+        }
+        val flutterView = (splashView as ViewGroup).getChildAt(0)
+        if (!isNonEmptyContainer(flutterView)) {
+            return false
+        }
+        val surfaceView = (flutterView as ViewGroup).getChildAt(0)
+        if (surfaceView !is SurfaceView) {
+            return false
+        }
+        surfaceView.setSecure(true)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+        return true
+    }
+
+    private fun isNonEmptyContainer(view: View): Boolean {
+        if (view !is ViewGroup) {
+            return false
+        }
+        if (view.childCount < 1) {
+            return false
+        }
+        return true
+    }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
